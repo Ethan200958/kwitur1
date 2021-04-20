b742ace9c2729b50e426226096f160e500a98e24
Ethan200958 <78169697+Ethan200958@users.noreply.github.com>
2021-04-19 20:35:43 -0500

    Add files via upload

---
+++ kwitur.js
@@ -0,0 +1,5 @@
+function addUsername() {
+    var username = document.getElementById("user_name").value;
+    localStorage.setItem("user_name", username);
+    window.location = "kwiturROOM.html";
+}
